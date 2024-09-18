14fdf751846b304e1393d5bb8794a7f332559b16
Dmitry Podrez <dima.padrez.00@icloud.com>
2024-09-18 10:16:57 +0200

    new file:   cipher-code.py   init: variables and structure

---
+++ cipher-code.py
@@ -0,0 +1,12 @@
+print('Hello, I will help you to encrypt or decrypt your text')
+print('Now, ask the question what do you need? - encryption \ decryption')
+answer = input()
+print('What language do you need? - ru \ en')
+lang = input()
+print('Step shift ?')
+step = int(input())
+
+alpha_ru = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+alpha_lower_ru = "абвгдежзийклмнопрстуфхцчшщъыьэюя"
+alpha_en = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+alpha_lower_en = "abcdefghijklmnopqrstuvwxyz"
