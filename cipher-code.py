7fa0fc7cd74d26753ec7ac4e130faa0dae9ec728
Dmitry Podrez <dima.padrez.00@icloud.com>
2024-09-19 07:04:11 +0200

    modified:   cipher-code.py

--- cipher-code.py
+++ cipher-code.py
@@ -1,12 +1,56 @@
 print('Hello, I will help you to encrypt or decrypt your text')
-print('Now, ask the question what do you need? - encryption \ decryption')
-answer = input()
-print('What language do you need? - ru \ en')
-lang = input()
-print('Step shift ?')
+print('Now, ask the question what do you need? - encryption / decryption')
+answer = input().lower()
+print('What language do you need? - ru / en')
+lang = input().lower()
+print('Step shift?')
 step = int(input())
+
 
 alpha_ru = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
 alpha_lower_ru = "абвгдежзийклмнопрстуфхцчшщъыьэюя"
 alpha_en = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 alpha_lower_en = "abcdefghijklmnopqrstuvwxyz"
+
+
+print('Your message:')
+message = input()
+
+def cipher(strr, step):
+    result = ''
+    if lang == 'ru':
+        for char in strr:
+            if char.isalpha():  
+                if char.isupper():
+                    ind = alpha_ru.find(char)
+                    number = (ind + step) % 32  
+                    result += alpha_ru[number]
+                else:
+                    ind = alpha_lower_ru.find(char)
+                    number = (ind + step) % 32
+                    result += alpha_lower_ru[number]
+            else:
+                result += char  
+
+    elif lang == 'en':
+        for char in strr:
+            if char.isalpha():
+                if char.isupper():
+                    ind = alpha_en.find(char)
+                    number = (ind + step) % 26
+                    result += alpha_en[number]
+                else:
+                    ind = alpha_lower_en.find(char)
+                    number = (ind + step) % 26
+                    result += alpha_lower_en[number]
+            else:
+                result += char  
+
+    print('Result:', result)
+
+if answer == 'encryption':
+    cipher(message, step)
+elif answer == 'decryption':
+    cipher(message, -step)
+else:
+    print("Invalid option. Please choose 'encryption' or 'decryption'.")
